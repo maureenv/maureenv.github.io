c9427a8f52c863a95bf22ff922e04ab6e3c7aad3
maureenv <maureenvogel@outlook.com>
2020-09-23 23:01:50 -0400

    Run build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4ed0a729eb79b885b75418bcd21fec7.js"
+  "/precache-manifest.3b6a5850ce54eb110cf2ee8e0e47af82.js"
 );
 
 self.addEventListener('message', (event) => {
