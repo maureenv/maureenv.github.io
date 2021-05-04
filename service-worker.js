4acbb633f748c6fd774fe46fccc6f10efde89093
maureenv <maureenvogel@outlook.com>
2021-05-04 13:14:06 -0400

    add new build files

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5afec049a8d0f8aedccd5246a041016b.js"
+  "/precache-manifest.3f23ec20cf41ccd6406f1a6e16b990c7.js"
 );
 
 self.addEventListener('message', (event) => {
